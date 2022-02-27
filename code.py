9dd94c3faa3fa4dde0e6cf96c93c9f8441488c13
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2022-02-27 16:10:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+browser = webdriver.Chrome("/Users/Mohit/Desktop/chromedriver")
+browser.get(START_URL)
+time.sleep(10)
+def scrape():
+    headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+    planet_data = []
+    
+scrape()
